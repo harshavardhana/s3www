4475d367310cf8845a2ac852a97c77cc5c5bb3c6
Harshavardhana <harsha@minio.io>
2020-01-11 16:16:31 -0800

    Add goreleaser

--- go12min.go
+++
@@ -1,8 +0,0 @@
-//+build !go1.12
-
-package main
-
-// This file is here to give a better hint in the error message
-// when this project is built with a too old version of Go.
-
-var _ = ThisProjectRequiresGo1·2OrHigher
